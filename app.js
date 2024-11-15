ed78ef0a677348e4afa469c2ba606d960277c446
Echefula Ndukwe <111073744+Eche5@users.noreply.github.com>
2024-11-15 23:14:33 +0100

    added rate limiter

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const indexRoutes = require("./routes/index");
 const userRoutes = require("./routes/user");
 const adminRoutes = require("./routes/admin");
+const rateLimit = require("express-rate-limit");
 
 const dotenv = require("dotenv").config();
 
@@ -12,9 +13,16 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+const authRateLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per windowMs
+  message: "Too many requests from this IP, please try again later.",
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 
 app.use(cors({ origin: true, credentials: true }));
-app.use("/api/v1/auth", authroutes);
+app.use("/api/v1/auth", authRateLimiter, authroutes);
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1", indexRoutes);
 app.use("/api/v1/admin", adminRoutes);
