b36981f87fcadc7ef43cb0ec34fac5f003e3f127
Echefula Ndukwe <111073744+Eche5@users.noreply.github.com>
2025-01-18 19:37:16 +0100

    fixed sql error

--- utils/database.js
+++ utils/database.js
@@ -1,12 +1,24 @@
 const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(
-  `${process.env.DB_NAME}`,
-  `${process.env.DB_USER}`,
-  `${process.env.DB_PASSWORD}`,
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
     dialect: "mysql",
-    host: `${process.env.DB_HOST}`,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306, 
+    logging: console.log, 
+    dialectOptions: {
+      connectTimeout: 60000, 
+    },
+    pool: {
+      max: 5, 
+      min: 0, 
+      acquire: 60000, 
+      idle: 10000,
+    },
   }
 );
+
 module.exports = sequelize;
